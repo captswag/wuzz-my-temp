659e1f467a4cd6ef12d72c51343b46bece375390
captswag <iamanjithsasindran@gmail.com>
2021-06-05 20:55:01 +0530

    Add sensors sample code from pysensors documentation

--- log.py
+++ log.py
@@ -9,7 +9,14 @@ GPU_SENSOR = 'amdgpu-pci-0a00'
 
 
 def startLogging():
-    print("Start logging")
+    sensors.init()
+    try:
+        for chip in sensors.iter_detected_chips():
+            print('%s at %s' % (chip, chip.adapter_name))
+            for feature in chip:
+                print(' %s: %.2f' % (feature.label, feature.get_value()))
+    finally:
+        sensors.cleanup()
 
 
 if __name__ == '__main__':
