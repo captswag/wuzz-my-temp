87d863922071b62282b57280f63c18ce3dc28a06
captswag <iamanjithsasindran@gmail.com>
2021-06-05 23:49:37 +0530

    Create function log_sensor_data()

--- log.py
+++ log.py
@@ -5,19 +5,23 @@ import sensors
 from tinydb import TinyDB, Query
 from datetime import datetime
 
-CPU_SENSOR = 'k10temp-pci-00c3'
-GPU_SENSOR = 'amdgpu-pci-0a00'
+CPU_SENSOR_NAME = 'k10temp-pci-00c3'
+GPU_SENSOR_NAME = 'amdgpu-pci-0a00'
 
 SCRIPT_DURATION = 10*60
 
 
 def start_logging():
     sensors.init()
+
+    # Get the required chip objects, which in our case is the CPU and GPU chips
+    # Call function log_chip_information() which starts a timer for the required SCRIPT_DURATION and
+    # handles the subsequent cases.
+    # Once the SCRIPT_DURATION is complete, the program gives control back to start_logging() where
+    # sensors.cleanup() happens.
+
     try:
-        for chip in sensors.iter_detected_chips():
-            print('%s at %s' % (chip.__str__(), chip.adapter_name))
-            for feature in chip:
-                print(' %s: %.2f' % (feature.label, feature.get_value()))
+        log_sensor_data(sensors.iter_detected_chips())
     finally:
         sensors.cleanup()
 
@@ -25,6 +29,13 @@ def start_logging():
 def get_db_file_name():
     now = datetime.now()
     return 'db-{datetime}.json'.format(datetime=now.strftime("%d-%m-%Y-%H-%M"))
+
+
+def log_sensor_data(detected_chips):
+    for chip in detected_chips:
+        print('%s at %s' % (chip.__str__(), chip.adapter_name))
+        for feature in chip:
+            print(' %s: %.2f' % (feature.label, feature.get_value()))
 
 
 if __name__ == '__main__':
