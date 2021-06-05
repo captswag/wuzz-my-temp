1ed81a5d91e0698aeb58961ccb05a71478eb44ff
captswag <iamanjithsasindran@gmail.com>
2021-06-05 21:13:29 +0530

    Convert chip to chip.__str__()

--- log.py
+++ log.py
@@ -12,7 +12,7 @@ def start_logging():
     sensors.init()
     try:
         for chip in sensors.iter_detected_chips():
-            print('%s at %s' % (chip, chip.adapter_name))
+            print('%s at %s' % (chip.__str__(), chip.adapter_name))
             for feature in chip:
                 print(' %s: %.2f' % (feature.label, feature.get_value()))
     finally:
