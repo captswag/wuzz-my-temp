028ccec3c3fe4f9b7d664d136265e6bebc3b8c49
captswag <iamanjithsasindran@gmail.com>
2021-06-06 13:10:51 +0530

    Log only the information of the required chips

--- log.py
+++ log.py
@@ -14,23 +14,28 @@ SCRIPT_DURATION = 10*60
 def start_logging():
     sensors.init()
 
-    # Get the required chip objects, which in our case is the CPU and GPU chips
-    # Call function log_chip_information() which starts a timer for the required SCRIPT_DURATION and
-    # handles the subsequent cases.
-    # Once the SCRIPT_DURATION is complete, the program gives control back to start_logging() where
-    # sensors.cleanup() happens.
+    # Get the required chip objects, which in our case is the CPU and GPU chips (get_required_chips)
+    # Start a timer which runs for the required SCRIPT_DURATION
+    # Log the feature label/value pair (log_sensor_data)
+    # Once the SCRIPT_DURATION is complete, the control is given back to start_logging() where sensor.cleanup() happens
 
     try:
         required_sensor_names = [CPU_SENSOR_NAME, GPU_SENSOR_NAME]
         required_chips = get_required_chips(
             required_sensor_names, sensors.iter_detected_chips())
-        log_sensor_data(sensors.iter_detected_chips())
+        log_sensor_data(required_chips)
     finally:
         sensors.cleanup()
 
 
 def get_required_chips(required_sensor_names, detected_chips):
-    return "required chips"
+    required_chips = []
+    for chip in detected_chips:
+        for sensor_name in required_sensor_names:
+            if (chip.__str__() == sensor_name):
+                required_chips.append(chip)
+                break
+    return required_chips
 
 
 def get_db_file_name():
